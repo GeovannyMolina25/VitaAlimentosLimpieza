de685b9fa8323759048d7783daa525f08e4086b2
Geovanny <geovannysangucho25@gmail.com>
2023-09-15 14:24:28 -0600

    main

---
+++ LimpiezaProyect/Controllers/RegistroResponsableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LimpiezaProyect.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
