04ffeeae67b3e1cde1c60049d268f06581722b2a
Nelson Molina <geovannysangucho25@gmail.com>
2023-09-20 22:38:19 -0500

    Envio de datos entre formularios

--- LimpiezaProyect/Controllers/ResponsableFormController.cs
+++ LimpiezaProyect/Controllers/ResponsableFormController.cs
@@ -11,85 +11,65 @@ namespace LimpiezaProyect.Controllers
     public class ResponsableFormController : Controller
     {
         private readonly LimpiezaContext _context;
-
         public ResponsableFormController(LimpiezaContext context)
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(string CodFormulario, string CodArea)
+        public async Task<IActionResult> Index(string CodFormulario, string CodArea, string CodEmpresa)
         {
             var codResgistros = await _context.LimpRegistros.Where(x => x.CodFormulario == CodFormulario).ToListAsync();
 
             TempData["Area"] = CodArea.ToString();
             TempData["Formulario"] = CodFormulario.ToString();
+            TempData["Empresa"] = CodEmpresa.ToString();
             return View(codResgistros);
         }
 
-        public IActionResult CreateForm()
-        {
-            ViewData["Registros"] = new SelectList(_context.LimpRegistros, "CodRegistro", "CodRegistro");
-            return View();
-        }
+        
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-    public async Task<IActionResult> CreateForm(LimpRegistro model)
-    {
-        var datos = await _context.LimpFormularios
-            .Where(r => r.CodArea == r.CodArea && r.CodEmpresa == "PQSA")
-            .FirstOrDefaultAsync();
-            if (datos == null)
+        public async Task<IActionResult> CreateForm(LimpRegistro model, string CodArea, string CodFormulario,string CodEmpresa)
             {
 
-            }
+                var registros = new LimpRegistro()
+                {
+                    CodArea = CodArea,
+                    FechaHoraCreacion = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                    CodEmpresa = CodEmpresa,
+                    CreadoPor = "Gmolina",
+                    Turno = model.Turno,
+                    FechaHoraRevisado = DateTime.Now,
+                    CodFormulario = CodFormulario,
+                    Estado = "1",
+                };
 
-            if (datos != null)
-        {
+                _context.Add(registros);
+                await _context.SaveChangesAsync();
+                TempData["CodArea"] = CodArea;
+                TempData["odFormulario"] = CodFormulario;
+                var registrosActualizados = await _context.LimpRegistros
+                    .Where(r => r.CodFormulario == CodFormulario) 
+                    .ToListAsync();
 
-            var registros = new LimpRegistro()
-            {
-                CodArea = datos.CodArea,
-                FechaHoraCreacion = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
-                CodEmpresa = datos.CodEmpresa,
-                CreadoPor = "Gmolina",
-                Turno = model.Turno,
-                FechaHoraRevisado = DateTime.Now,
-                CodFormulario = datos.CodFormulario,
-                Estado = "1",
-            };
-
-            _context.Add(registros);
-            await _context.SaveChangesAsync();
-                
-            var registrosActualizados = await _context.LimpRegistros
-                .Where(r => r.CodArea == datos.CodArea) 
-                .ToListAsync();
-
-            return View("Index", registrosActualizados);
+                return View("Index", registrosActualizados);
         }
 
-        ViewData["Registros"] = new SelectList(_context.LimpRegistros, "CodRegistro", "CodRegistro");
-        return View(model);
-    }
-
-        [HttpPost]
-        public async Task<IActionResult> EliminarRegistro(int NumFormulario)
-        {
-            var registroAEliminar = await _context.LimpRegistros.FirstOrDefaultAsync(x => x.NumFormulario == NumFormulario);
-
-            if (registroAEliminar == null)
+            [HttpPost]
+            public async Task<IActionResult> EliminarRegistro(int NumFormulario)
             {
-                return NotFound();
+                var registroAEliminar = await _context.LimpRegistros.FirstOrDefaultAsync(x => x.NumFormulario == NumFormulario);
+
+                if (registroAEliminar == null)
+                {
+                    return NotFound();
+                }
+
+                _context.LimpRegistros.Remove(registroAEliminar);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", new { CodFormulario = registroAEliminar.CodFormulario });
             }
-
-            _context.LimpRegistros.Remove(registroAEliminar);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("Index", new { CodFormulario = registroAEliminar.CodFormulario });
         }
-
-
-
-    }
 }
