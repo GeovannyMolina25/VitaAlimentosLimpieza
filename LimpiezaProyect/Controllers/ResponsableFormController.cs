4b3ab58ec2cafbf1c0dcc643882a791812785d67
Geovanny <geovannysangucho25@gmail.com>
2023-09-21 08:19:29 -0600

    Cambios boton eliminar

--- LimpiezaProyect/Controllers/ResponsableFormController.cs
+++ LimpiezaProyect/Controllers/ResponsableFormController.cs
@@ -19,71 +19,89 @@ namespace LimpiezaProyect.Controllers
         {
             var codResgistros = await _context.LimpRegistros.Where(x => x.CodFormulario == CodFormulario).ToListAsync();
 
-            TempData["Area"] = CodArea.ToString();
-            TempData["Formulario"] = CodFormulario.ToString();
-            TempData["Empresa"] = CodEmpresa.ToString();
+            TempData["Area"] = CodArea != null ? CodArea.ToString() : null;
+            TempData["Formulario"] = CodFormulario != null ? CodFormulario.ToString() : null;
+            TempData["Empresa"] = CodEmpresa != null ? CodEmpresa.ToString() : null;
+
             return View(codResgistros);
         }
 
-        
+        public async Task<IActionResult> CreateForm(string CodArea, string CodFormulario, string CodEmpresa)
+        {
+            TempData["Area"] = CodArea.ToString();
+            TempData["Formulario"] = CodFormulario.ToString();
+            TempData["Empresa"] = CodEmpresa.ToString();
+            return View();
+        }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
         public async Task<IActionResult> CreateForm(LimpRegistro model, string CodArea, string CodFormulario,string CodEmpresa)
+        {
+            if (ModelState.IsValid)
             {
-            var horaActual = DateTime.Now.TimeOfDay;
-            string turno = "";
-
-            if (horaActual >= TimeSpan.Zero && horaActual < TimeSpan.FromHours(12))
-            {
-                turno = "Mañana";
-            }
-            else if (horaActual >= TimeSpan.FromHours(12) && horaActual < TimeSpan.FromHours(19))
-            {
-                turno = "Tarde";
-            }
-            else
-            {
-                turno = "Noche";
-            }
-            var registros = new LimpRegistro()
+              
+                    if (CodArea != null && CodFormulario != null && CodEmpresa != null)
                 {
-                    CodArea = CodArea,
-                    FechaHoraCreacion = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
-                    CodEmpresa = CodEmpresa,
-                    CreadoPor = "Gmolina",
-                    Turno = turno,
-                    FechaHoraRevisado = DateTime.Now,
-                    CodFormulario = CodFormulario,
-                    Estado = "1",
-                };
+                    var horaActual = DateTime.Now.TimeOfDay;
+                    string turno = "";
 
-                _context.Add(registros);
-                await _context.SaveChangesAsync();
-                TempData["CodArea"] = CodArea;
-                TempData["odFormulario"] = CodFormulario;
-                var registrosActualizados = await _context.LimpRegistros
-                    .Where(r => r.CodFormulario == CodFormulario) 
-                    .ToListAsync();
+                    if (horaActual >= TimeSpan.Zero && horaActual < TimeSpan.FromHours(12))
+                    {
+                        turno = "Mañana";
+                    }
+                    else if (horaActual >= TimeSpan.FromHours(12) && horaActual < TimeSpan.FromHours(19))
+                    {
+                        turno = "Tarde";
+                    }
+                    else
+                    {
+                        turno = "Noche";
+                    }
+                    var registros = new LimpRegistro()
+                    {
+                        CodArea = CodArea,
+                        FechaHoraCreacion = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                        CodEmpresa = CodEmpresa,
+                        CreadoPor = "Gmolina",
+                        Turno = turno,
+                        FechaHoraRevisado = DateTime.Now,
+                        CodFormulario = CodFormulario,
+                        Estado = "1",
+                    };
 
-                return View("Index", registrosActualizados);
+                    _context.Add(registros);
+                    await _context.SaveChangesAsync();
+                    TempData["CodArea"] = CodArea;
+                    TempData["odFormulario"] = CodFormulario;
+                    var registrosActualizados = await _context.LimpRegistros
+                        .Where(r => r.CodFormulario == CodFormulario)
+                        .ToListAsync();
+
+                    return View("Index", registrosActualizados);
+                }
+            }
+
+                ViewData["Registros"] = new SelectList(_context.LimpRegistros, "CodRegistro", "CodRegistro");
+                return View();  
         }
 
             [HttpPost]
-            public async Task<IActionResult> EliminarRegistro(int NumFormulario)
+        public async Task<IActionResult> EliminarRegistro(string CodArea, int NumFormulario, string CodEmpresa)
+        {
+            var registroAEliminar = await _context.LimpRegistros.FirstOrDefaultAsync(x => x.NumFormulario == NumFormulario);
+
+            if (registroAEliminar == null)
             {
-                var registroAEliminar = await _context.LimpRegistros.FirstOrDefaultAsync(x => x.NumFormulario == NumFormulario);
-
-                if (registroAEliminar == null)
-                {
-                    return NotFound();
-                }
-
-                _context.LimpRegistros.Remove(registroAEliminar);
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction("Index", new { CodFormulario = registroAEliminar.CodFormulario });
+                return NotFound();
             }
+
+            _context.LimpRegistros.Remove(registroAEliminar);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", new { CodFormulario = registroAEliminar.CodFormulario, CodArea = CodArea,CodEmpresa = CodEmpresa });
         }
+    }
 }
